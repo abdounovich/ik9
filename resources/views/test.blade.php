eb2d628ef51189d1b3c0d2ba94de27bdd6ba1b9d
1 <abdounovich@gmail.com>
2020-09-18 12:42:59 +0200

    'comment'

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -20,27 +20,24 @@
 
     <button type="button" class="button" onclick="sendMessage();">Complete Booking</button>
 
-    <script type="text/javascript">
-       
-
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) { return; }
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, "script", "Messenger"));
-
-        window.extAsyncInit = function () {
-            // the Messenger Extensions JS SDK is done loading
-            MessengerExtensions.getUserID(function success(uids) {
-                var psid = uids.psid;//This is your page scoped sender_id
-                alert("Getting PSID")
-                alert("This is the user's psid " + psid);
-            }, function error(err) {
-                alert("Messenger Extension Error: " + err);
-            });
+    <script>
+        (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
+        fjs.parentNode.insertBefore(js, fjs);
+         }(document, "script", "Messenger"));
+        
+          window.extAsyncInit = function () {
+          // the Messenger Extensions JS SDK is done loading
+           MessengerExtensions.getUserID(function success(uids) {
+            var psid = uids.psid;//This is your page scoped sender_id
+            alert(psid);
+        }, function error(err) {
+            alert("Messenger Extension Error: " + err);
+        });
         };
-    </script>
+        </script>  
 </body>
 </html>
