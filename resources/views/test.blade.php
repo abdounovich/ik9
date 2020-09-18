031a32a1da4df4b8cf69e7e82b9e1115c02fcb96
1 <abdounovich@gmail.com>
2020-09-18 15:47:39 +0200

    'comment'

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -17,10 +17,10 @@
             margin: 25%;
         }
     </style>
-<form id="myForm" method="post" action="/test2">
+<form id="myForm" method="POST" action="/test2">
     @csrf
     message <input type="text" name="message"><br>
-    <input type="submit"  value="Submit form">
+    <input type="button" onclick="sendMessage()" value="Submit form">
   </form>
 
     <script type="text/javascript">
@@ -28,10 +28,29 @@
             document.getElementById("myForm").submit();
 
         
-         
+            MessengerExtensions.requestCloseBrowser(function success() {
+
+            }, function error(err) {
+
+            });
         }
 
-  
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) { return; }
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, "script", "Messenger"));
+
+        window.extAsyncInit = function () {
+            // the Messenger Extensions JS SDK is done loading
+            MessengerExtensions.getUserID(function success(uids) {
+                var psid = uids.psid;//This is your page scoped sender_id
+            }, function error(err) {
+                alert("Messenger Extension Error: " + err);
+            });
+        };
     </script>
 </body>
 </html>
